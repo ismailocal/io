a359458f5f2aa5dec0a9b44b89571cefb458cd43
İsmail Öcal <ismailocal@gmail.com>
2019-06-24 17:07:47 +0300

    .

--- README.md
+++ README.md
@@ -3,50 +3,46 @@
 ## variable
 
 ```javascript
+// define variable
 io('config', {
     env: 'dev',
     url: 'http://localhost/'
 });
+
+// console variable
+console.log(io('config'));
 ```
 
 ### module
 ```javascript
+// define module
 io('my-module', function () {
         console.log('my-module');
-    })
-    .run();
-```
-or
-```javascript
-io('my-module').run(function () {
-    console.log('my-module');
-});
+    });
+
+// run module
+io('my-module');
 ```
 
 ### module inject another module or variable
 ```javascript
+// define module
 io('my-module', function (config) {
         console.log(config.env);
-    })
-    .inject('config')
-    .run();
-```
-or
-```javascript
-io('my-module')
-    .inject('config')
-    .run(function (config) {
-        console.log(config.env);
     });
+
+// run module
+io('my-module');
 ```
 ### use module another way
 ```javascript
+// define module
 io('my-module', function (config) {
         this.log = function(){
             console.log(config.env);
         }
-    })
-    .inject('config');
+    });
 
+// run module and use method
 io('my-module').log();
 ```
--- io.js
+++ io.js
@@ -1,58 +1,53 @@
 var io = (function () {
 
-    var self = {
-        modules: {},
-        module: {
-            name: 'default'
-        }
-    };
+    this.modules = {};
 
-    this.run = function (name) {
-        if (typeof self.modules[name].value === 'function') {
-            var injects = [];
-            self.modules[name].injects.map(function (inject) {
-                if (!self.modules[inject].object) {
-                    this.run(inject);
-                }
-                injects.push(self.modules[inject].object);
-            }.bind(this));
-
-            return self.modules[name].object = new (Function.prototype.bind.apply(self.modules[name].value, [null].concat(injects)));
-        } else {
-            return self.modules[name].object = self.modules[name].value;
-        }
-    };
-
-    return function (name, value) {
-
-        if (self.modules[name] && !value) {
-            return this.run(self.module.name);
-        }
-
-        self.modules[self.module.name = name] = {
+    this.init = function (name, value) {
+        this.modules[name] = {
             injects: [],
             extends: [],
             object: null,
             value: value
         };
+    };
 
-        return {
-            inject: function () {
-                [].slice.call(arguments).map(function (inject) {
-                    self.modules[self.module.name].injects.push(inject);
-                });
-                return this;
-            },
-            wait: function (callable) {
-                self.modules[self.module.name].value = callable;
-                return this;
-            },
-            run: function (callable) {
-                if (callable) {
-                    self.modules[self.module.name].value = callable;
+    this.args = function (fn) {
+        return fn.toString()
+            .replace(/((\/\/.*$)|(\/\*[\s\S]*?\*\/)|(\s))/mg, '')
+            .match(/^function\s*[^\(]*\(\s*([^\)]*)\)/m)[1]
+            .split(/,/);
+    };
+
+    this.create = function (name) {
+        if (this.modules[name].object) {
+            return this.modules[name].object;
+        }
+
+        if (typeof this.modules[name].value === 'function') {
+            this.modules[name].injects = this.args(this.modules[name].value);
+            var injects = [];
+            this.modules[name].injects.map(function (inject) {
+                if (!this.modules[inject].object) {
+                    this.create(inject);
                 }
-                this.run(self.module.name);
-            }.bind(this)
-        };
+                injects.push(this.modules[inject].object);
+            }.bind(this));
+
+            return this.modules[name].object = new (Function.prototype.bind.apply(this.modules[name].value, [null].concat(injects)));
+        } else {
+            return this.modules[name].object = this.modules[name].value;
+        }
+    };
+
+    return function () {
+
+        var args = [].slice.call(arguments);
+
+        switch (args.length) {
+            case 1:
+                return this.create(args[0]);
+            case 2:
+                return this.init(args[0], args[1]);
+        }
     };
 })();
